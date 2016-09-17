930a362ade98fed7237e8a344b9809ee31fbed6f
maru <wsj7178@gmail.com>
2016-09-17 22:55:34 +0900

    Now you can fix message!

--- src/maru/restarter/Main.java
+++ src/maru/restarter/Main.java
@@ -2,18 +2,32 @@ package maru.restarter;
 
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.scheduler.AsyncTask;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.ConfigSection;
 
 public class Main extends PluginBase {
-
+	private Config config;
+	
+	@SuppressWarnings("serial")
 	@Override
 	public void onEnable() {
+		getDataFolder().mkdirs();
+		config = new Config(getDataFolder() + "/config.yml", Config.YAML, new ConfigSection() {
+			{
+				put("restart-message", "Start server restarting cause memory leak!");
+			}
+		});
+		
 		getServer().getScheduler().scheduleAsyncTask(new AsyncTask() {
 
 			@Override
 			public void onRun() {
 				while (true) {
 					if (Runtime.getRuntime().totalMemory() / 1024 / 1024 > (Runtime.getRuntime().maxMemory() / 1024 / 1024) - 256) {
-						getLogger().critical("Start server restarting cause memory leak!");
+						getLogger().critical(config.getString("restart-message"));
+						getServer().getOnlinePlayers().forEach((uuid, player) -> {
+							player.kick(config.getString("restart-message"), false);
+						});
 						getServer().forceShutdown();
 					}
 					try {
