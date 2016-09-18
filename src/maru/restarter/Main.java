4a84d8c2d905d33dfff922d289ec06408469b533
maru <wsj7178@gmail.com>
2016-09-18 12:20:11 +0900

    change check milisec

--- src/maru/restarter/Main.java
+++ src/maru/restarter/Main.java
@@ -31,7 +31,7 @@ public class Main extends PluginBase {
 						getServer().forceShutdown();
 					}
 					try {
-						Thread.sleep(100);
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 						// ignore
 					}
